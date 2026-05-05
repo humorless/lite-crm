9b42b0ba6aa79581f2e37cc9973f7e27308e28b4
Laurence Chen <laurence@replware.dev>
2026-05-05 13:39:41 +0800

    Initial commit

---
+++ src/lite_crm/core.clj
@@ -0,0 +1,9 @@
+(ns lite-crm.core
+  (:gen-class)
+  (:require [integrant-extras.core :as ig-extras]))
+
+(defn -main
+  "Run application system in production."
+  []
+  (ig-extras/run-system {:profile :prod
+                         :config-path "config.edn"}))
---
+++ src/lite_crm/handlers.clj
@@ -0,0 +1,19 @@
+(ns lite-crm.handlers
+  (:require [reitit-extras.core :as reitit-extras]
+            [ring.util.response :as response]
+            [lite-crm.views :as views]))
+
+(defn default-handler
+  [error-text status-code]
+  (fn [_]
+    (-> (views/error-page error-text)
+        (reitit-extras/render-html)
+        (response/status status-code))))
+
+(defn home-handler
+  [{router :reitit.core/router
+    :as request}]
+  (-> {:user (:identity request)
+       :router router}
+      (views/home-page)
+      (reitit-extras/render-html)))
